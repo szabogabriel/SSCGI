66ec20404d2461347f2ed151c134be10dbab3cf4
gabor <szabo.gabriel@gmail.com>
2021-03-15 00:41:19 +0100

    Changed gitignore and add maven nature and fix echo demo server

--- SSCGI/src/sscgi/demo/EchoServer.java
+++ SSCGI/src/sscgi/demo/EchoServer.java
@@ -1,21 +1,35 @@
 package sscgi.demo;
 
+import java.io.IOException;
+
+import sscgi.SSCGIMessage;
+import sscgi.server.SSCGIServer;
+
 public class EchoServer {
 	
 	public static void main(String [] args) {
+		int port = 65000;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				System.err.println("The input parameter must be the server port.");
+				System.exit(-1);
+			}
+		}
+		
+		System.out.println("Starting server on port: " + port);
 		try {
-			new SSCGIServer(65000, req -> new SSCGIMessage(new byte [] {}, ("Hello, " + new String(req.getBody())).getBytes()))));
+			new SSCGIServer(port, EchoServer::handleRequest);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
-		while (true) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+	}
+	
+	private static SSCGIMessage handleRequest(SSCGIMessage req) {
+		String header = "";
+		String body = "Hello, " + new String(req.getBody());
+		return new SSCGIMessage(header.getBytes(), body.getBytes());
 	}
 
 }
